a0c657c135c3ea079094c18e7173bae876630c67
Yuriy Dvorzhetskiy <dvorzhetskij@mail.ru>
2021-06-14 10:19:36 +0600

    Initial

---
+++ src/main/java/com/dxc/poc/beam/pipeline/GsToBqOptions.java
@@ -0,0 +1,25 @@
+package com.dxc.poc.beam.pipeline;
+
+import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
+import org.apache.beam.sdk.options.*;
+
+public interface GsToBqOptions extends DataflowPipelineOptions {
+
+    @Description("Path of the file to read from")
+    @Validation.Required
+    String getInputFile();
+
+    void setInputFile(String value);
+
+    @Description("Output dataset ID")
+    @Validation.Required
+    String getDataset();
+
+    void setDataset(String value);
+
+    @Description("Output table name")
+    @Default.String("pnr_output")
+    String getTableName();
+
+    void setTableName(String value);
+}
---
+++ src/main/java/com/dxc/poc/beam/pipeline/PnrGsToBqPipeline.java
@@ -0,0 +1,69 @@
+package com.dxc.poc.beam.pipeline;
+
+import com.dxc.poc.beam.dto.Pnr;
+import com.sabre.gcp.logging.CloudLogger;
+import com.sabre.gcp.validation.ValidationChain;
+import com.sabre.gcp.validation.ValidationUtils;
+import com.sabre.gcp.validation.XmlSchemaValidator;
+import lombok.val;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.coders.SerializableCoder;
+import org.apache.beam.sdk.extensions.jackson.ParseJsons;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
+import org.apache.beam.sdk.metrics.MetricNameFilter;
+import org.apache.beam.sdk.metrics.MetricResult;
+import org.apache.beam.sdk.metrics.MetricsFilter;
+import org.apache.beam.sdk.options.ValueProvider;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollectionTuple;
+
+import static com.dxc.poc.beam.utils.MetricUtils.withElapsedTime;
+import static com.sabre.gcp.validation.ValidationUtils.invalidTag;
+import static com.sabre.gcp.validation.ValidationUtils.successTag;
+
+public class PnrGsToBqPipeline {
+
+    private static final CloudLogger logger = CloudLogger.getLogger(PnrGsToBqPipeline.class);
+
+    public static void createAndRunPipeline(GsToBqOptions options) {
+        val tableRef = BqMetadataFactory.createTableReference(
+            options.getProject(), options.getDataset(), options.getTableName());
+        val schema = BqMetadataFactory.createTableSchema();
+
+        Pipeline.create(options)
+            .apply("Read from file", TextIO.read().from(options.getInputFile()))
+            // Converts to POJO or GenericRecord with the ticketNumber field
+            .apply("Convert to POJO", ParDo.of(ConverToPojo.class))
+            // Removes the field tickerNumber and adds saves encrypted version to "_enc"
+            .apply(Deidentifying.propertyFromTo("ticketNumber", "_enc")
+                    .withEncryptionDetails("projectId", "appName", "keyRingId", "keyId"))
+            .apply("Convert to table row",
+                withElapsedTime("to_table_row_milliseconds", ParDo.of(new ToTableRowDoFn())))
+            .apply("Write to BQ", BigQueryIO.writeTableRows()
+                .to(tableRef)
+                .withSchema(schema)
+                .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED)
+                .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND)
+                .withCustomGcsTempLocation(ValueProvider.StaticValueProvider.of(options.getGcpTempLocation())))
+            .getPipeline()
+            .run();
+
+        result.waitUntilFinish();
+
+        val metrics = result.metrics();
+
+        // Write all successful processed
+        val metricsResults = metrics.queryMetrics(MetricsFilter.builder()
+            .addNameFilter(MetricNameFilter.named("beam-example", "good_record_count")).build());
+
+        for (MetricResult<Long> counter : metricsResults.getCounters()) {
+            // Here is only one counter
+            val count = counter.getAttempted();
+            logger.info("Number of good records: " + count,
+                KV.of("stats", "good_records_processed"),
+                KV.of("count", count));
+        }
+    }
+}
