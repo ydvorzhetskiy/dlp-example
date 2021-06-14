a6a69150ae3a9b8afb4e341b022bb7e0af9bf646
Yuriy Dvorzhetskiy <dvorzhetskij@mail.ru>
2021-06-14 12:25:09 +0600

    credit card masking

--- src/main/java/com/dxc/poc/beam/dlp/CreditCardMasking.java
+++ src/main/java/com/dxc/poc/beam/dlp/CreditCardMasking.java
@@ -1,13 +1,59 @@
 package com.dxc.poc.beam.dlp;
 
-import com.dxc.poc.beam.dto.Pnr;
-import com.google.api.services.bigquery.model.TableRow;
+import com.google.cloud.dlp.v2.DlpServiceClient;
+import com.google.privacy.dlp.v2.*;
 import org.apache.beam.sdk.transforms.DoFn;
+
+import java.util.Arrays;
 
 public class CreditCardMasking extends DoFn<String, String> {
 
     @ProcessElement
-    public void processElement(@Element String original, OutputReceiver<String> out) {
-        out.output(original);
+    public void processElement(@Element String original, OutputReceiver<String> out) throws Exception {
+        try (DlpServiceClient dlp = DlpServiceClient.create()) {
+
+            // Specify what content you want the service to DeIdentify
+            ContentItem contentItem = ContentItem.newBuilder().setValue(original).build();
+
+            InfoType infoType = InfoType.newBuilder()
+                .setName("CREDIT_CARD_NUMBER").build();
+            InspectConfig inspectConfig =
+                InspectConfig.newBuilder().addAllInfoTypes(Arrays.asList(infoType)).build();
+
+            // Specify how the info from the inspection should be masked.
+            CharacterMaskConfig characterMaskConfig =
+                CharacterMaskConfig.newBuilder()
+                    .setMaskingCharacter("#")
+                    .setNumberToMask(12)
+                    .build();
+            PrimitiveTransformation primitiveTransformation =
+                PrimitiveTransformation.newBuilder()
+                    .setReplaceWithInfoTypeConfig(ReplaceWithInfoTypeConfig.getDefaultInstance())
+                    .build();
+            InfoTypeTransformations.InfoTypeTransformation infoTypeTransformation =
+                InfoTypeTransformations.InfoTypeTransformation.newBuilder()
+                    .setPrimitiveTransformation(primitiveTransformation)
+                    .build();
+            InfoTypeTransformations transformations =
+                InfoTypeTransformations.newBuilder().addTransformations(infoTypeTransformation).build();
+
+            DeidentifyConfig deidentifyConfig =
+                DeidentifyConfig.newBuilder().setInfoTypeTransformations(transformations).build();
+
+            // Combine configurations into a request for the service.
+            DeidentifyContentRequest request =
+                DeidentifyContentRequest.newBuilder()
+                    .setParent(LocationName.of("sabre-cdw-dev-sandbox", "us-central1").toString())
+                    .setItem(contentItem)
+                    .setInspectConfig(inspectConfig)
+                    .setDeidentifyConfig(deidentifyConfig)
+                    .build();
+
+            // Send the request and receive response from the service
+            DeidentifyContentResponse response = dlp.deidentifyContent(request);
+
+            out.output(response.getItem().getValue());
+        }
+
     }
 }
